e3cd8f82c4e80453cdc910ee70432b9085c04da0
mackybean <mackybean@msn.com>
2017-03-08 19:23:05 -0500

    Add files via upload

---
+++ plugins/Grimm/Grimm.js
@@ -0,0 +1,21 @@
+exports.commands = [
+    "grimm",
+	"Grimm"
+];
+
+
+
+
+exports.Grimm = {
+	description: "Grimm",
+	process: function(bot,msg){
+		msg.channel.sendMessage("http://spaceaim.com/wp-content/uploads/2015/08/1418824037space.jpg");
+	}
+}
+
+exports.grimm = {
+	description: "Grimm",
+	process: function(bot,msg){
+		msg.channel.sendMessage("http://spaceaim.com/wp-content/uploads/2015/08/1418824037space.jpg");
+	}
+}
---
+++ plugins/Phoe/Phoe.js
@@ -0,0 +1,21 @@
+exports.commands = [
+    "Phoe",
+	"phoe"
+];
+
+
+
+
+exports.Phoe = {
+	description: "Phoe <3",
+	process: function(bot,msg){
+		msg.channel.sendMessage("http://kingofwallpapers.com/phoenix/phoenix-006.jpg");
+	}
+}
+
+exports.phoe = {
+	description: "Phoe <3",
+	process: function(bot,msg){
+		msg.channel.sendMessage("http://kingofwallpapers.com/phoenix/phoenix-006.jpg");
+	}
+}
